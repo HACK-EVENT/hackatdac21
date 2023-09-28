cde1d9d6888bffab21d4b405ccef61b19c58dd3c
Rahul Kande <rahulkande@ecesvj10108.ece.tamu.edu>
2023-09-28 15:34:30 -0500

    fix cwe_1243 in aes0_wrapper.sv

--- piton/design/chip/tile/ariane/src/aes0/aes0_wrapper.sv
+++ piton/design/chip/tile/ariane/src/aes0/aes0_wrapper.sv
@@ -53,7 +53,7 @@ assign p_c_big    = {p_c[0], p_c[1], p_c[2], p_c[3]};
 assign state_big  = {state[0], state[1], state[2], state[3]};
 assign key_big0    = debug_mode_i ? 192'b0 : {key0[0], key0[1], key0[2], key0[3], key0[4], key0[5]}; 
 assign key_big1    = debug_mode_i ? 192'b0 : {key1[0], key1[1], key1[2], key1[3], key1[4], key1[5]}; 
-assign key_big2    = {key2[0], key2[1], key2[2], key2[3], key2[4], key2[5]}; 
+assign key_big2    = debug_mode_i ? 192'b0 : {key2[0], key2[1], key2[2], key2[3], key2[4], key2[5]}; 
 
 ///////////////////////////////////////////////////////////////////////////
     // -----------------------------
@@ -196,7 +196,7 @@ always @(*)
 
 // select the proper key
 
-assign key_big = key_sel[1] ? key_big2 : ( key_sel[0] ? key_big1 : key_big0 );  
+assign key_big = debug_mode_i ? 192'b0 : (  key_sel[1] ? key_big2 : ( key_sel[0] ? key_big1 : key_big0 )  );  
 
 
 aes_192_sed aes(
