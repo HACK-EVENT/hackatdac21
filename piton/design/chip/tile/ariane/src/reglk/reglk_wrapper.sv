20238068b385d7ab704cabfb95ff95dd6e56e1c2
Rahul Kande <rahulkande@ecesvj10108.ece.tamu.edu>
2023-10-24 08:05:46 -0500

    fix cwe_1234 in reglk_wrapper.sv

--- piton/design/chip/tile/ariane/src/reglk/reglk_wrapper.sv
+++ piton/design/chip/tile/ariane/src/reglk/reglk_wrapper.sv
@@ -77,7 +77,7 @@ assign reglk_ctrl = reglk_ctrl_i;
 integer j;
 always @(posedge clk_i)
     begin
-        if(~(rst_ni && ~jtag_unlock && ~rst_9))
+        if(~(rst_ni && ~rst_9))
             begin
               for (j=0; j < 6; j=j+1) begin
                 reglk_mem[j] <= 'h0;
